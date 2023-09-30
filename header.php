5bcdce6da239333e6064c0298ec2c4bbff08fb9c
Nikeshana <146467081+Nikeshana@users.noreply.github.com>
2023-10-01 00:59:15 +0530

    Create header.php

---
+++ header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="bootstrap.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+
+
+</head>
+
+<body>
+    <div class="col-12 bg-transparent">
+        <div class="row g-2">
+            <div class="col-12 col-lg-6">
+                <h1 class="fw-bold fs-1">Charity Donation</h1>
+            </div>
+            <div class="col-12 col-lg-6 text-center d-flex">
+                <nav class="mt-5">
+                    <ul>
+                        <li class="d-inline-flex"><a href="#">Home</a></li>
+                        <li class="d-inline-flex"><a href="#">About</a></li>
+                        <li class="d-inline-flex"><a href="#">Services</a></li>
+                        <li class="d-inline-flex"><a href="#">Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+
+        </div>
+
+    </div>
+</body>
+
+</html>
