962157f52a613f99c740cd9dcfbcaffcec3b47fa
Helitha Wijesooriya <143673858+Helitha1@users.noreply.github.com>
2023-09-30 21:28:12 +0530

    main

--- signIn.php
+++ signIn.php
@@ -13,7 +13,42 @@
     
  <div class="container-fluid bg-signIn "> 
     <div class="container"> 
-        
+    <div class="row">
+                <div class="col-lg-9 col-md-12 login-card">
+                    <div class="row">
+                        <div class="col-md-5 detail-part">
+                            <h1>Awesome Login Page Design</h1>
+                            <p>Please use your credentials to login.
+                                If you are not a member, please register. </p>
+                        </div>
+                        <div class="col-md-7 logn-part">
+                          <div class="row">
+                              <div class="col-lg-10 col-md-12 mx-auto">
+                                  <div class="logo-cover">
+                                       <img src="assets/images/logo.png" alt="">
+                                   </div>
+                                    <div class="form-cover">
+                                        <h6>Login Here</h6>
+                                         <input placeholder="Enter Username" type="text" class="form-control">
+                                         <input Placeholder="Enter PAssword" type="password" class="form-control">
+                                         <div class="row form-footer">
+                                             <div class="col-md-6 forget-paswd">
+                                                 <a href="">Forget Password ?</a>    
+                                             </div>
+                                             <div class="col-md-6 button-div">
+                                                 <button class="btn btn-primary">Login</button>
+                                             </div>
+                                         </div>
+                                    </div>
+                              </div>
+                          </div>
+                           
+                           
+                            
+                        </div>
+                    </div>
+                </div>
+            </div> 
     
 
     </div>
