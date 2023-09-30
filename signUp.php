a6ec2cc13224986d31ed3821d71d488bb57b727a
Chanuka <chanukab2020@gmail.com>
2023-09-30 21:33:55 +0530

    Update signUp.php

---
+++ signUp.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title> Sign up</title>
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" href="assets/images/fav.png">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <div class="container-fluid bg-login">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-9 col-md-12 login-card">
+                    <div class="row">
+                        <div class="col-md-5 detail-part">
+                            <h1>Awesome sign up Page Design</h1>
+                            <p>Please use your credentials to login. If you are not a member, please register. </p>
+                        </div>
+                        <div class="col-md-7 logn-part">
+                            <div class="row">
+                                <div class="col-lg-10 col-md-12 mx-auto">
+                                    <div class="logo-cover">
+                                        <img src="assets/images/logo.png" alt="">
+                                    </div>
+                                    <div class="form-cover">
+                                        <h6>Login Here</h6>
+                                        <input placeholder="Enter Firstname" type="text" class="form-control">
+                                        <input placeholder="Enter Lastname" type="text" class="form-control">
+                                        <input placeholder="Enter Mobile" type="text" class="form-control">
+                                        <input Placeholder="Enter PAssword" type="password" class="form-control">
+                                        <input Placeholder="Conferm PAssword" type="password" class="form-control">
+                                        <input placeholder="Enter Address Line 1" type="text" class="form-control">
+                                        <input placeholder="Enter Address Line 2" type="text" class="form-control">
+                                        <div class="row form-footer">
+                                            <!-- <div class="col-md-6 forget-paswd">
+                                                <a href="">Forget Password ?</a>
+                                            </div> -->
+                                            <div class="col-md-6 button-div">
+                                                <button class="btn btn-primary">Sign UP</button>
+                                                <button class="btn btn-primary">Back</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+<script src="assets/js/jquery-3.2.1.min.js"></script>
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+
+</html>
