99a43379740c7dce6ce410f94c8461a9d9b46e89
sasindu2002 <113811301+sasindu2002@users.noreply.github.com>
2023-10-03 22:43:29 +0530

    Update forgotPasswordProcess.php

--- forgotPasswordProcess.php
+++ forgotPasswordProcess.php
@@ -17,34 +17,32 @@ if(isset($_GET["e"])){
 
     if($n == 1){
 
-        echo("yes");
-
         $code = uniqid();
 
-        // Database::iud("UPDATE `users` SET `verification_code`='".$code."' WHERE 
-        // `email`='".$email."'");
+        Database::iud("UPDATE `users` SET `verification_code`='".$code."' WHERE 
+        `email`='".$email."'");
 
-        // $mail = new PHPMailer;
-        //     $mail->IsSMTP();
-        //     $mail->Host = 'smtp.gmail.com';
-        //     $mail->SMTPAuth = true;
-        //     $mail->Username = 'sasinduprasad2002@gmail.com';
-        //     $mail->Password = 'mjex zjzg jvbc byri';
-        //     $mail->SMTPSecure = 'ssl';
-        //     $mail->Port = 465;
-        //     $mail->setFrom('sasinduprasad2002gmail.com', 'Reset Password');
-        //     $mail->addReplyTo('sasinduprasad2002gmail.com', 'Reset Password');
-        //     $mail->addAddress($email);
-        //     $mail->isHTML(true);
-        //     $mail->Subject = 'Your Forgot Password Verification Code';
-        //     $bodyContent = '<h1 style="color:green">Your Verification code is '.$code.'</h1>';
-        //     $mail->Body    = $bodyContent;
+        $mail = new PHPMailer;
+            $mail->IsSMTP();
+            $mail->Host = 'smtp.gmail.com';
+            $mail->SMTPAuth = true;
+            $mail->Username = 'sasinduprasad2002@gmail.com';
+            $mail->Password = 'mjex zjzg jvbc byri';
+            $mail->SMTPSecure = 'ssl';
+            $mail->Port = 465;
+            $mail->setFrom('sasinduprasad2002gmail.com', 'Reset Password');
+            $mail->addReplyTo('sasinduprasad2002gmail.com', 'Reset Password');
+            $mail->addAddress($email);
+            $mail->isHTML(true);
+            $mail->Subject = 'Your Forgot Password Verification Code';
+            $bodyContent = '<h1 style="color:green">Your Verification code is '.$code.'</h1>';
+            $mail->Body    = $bodyContent;
 
-        //     if (!$mail->send()) {
-        //         echo 'Verification code sending failed';
-        //     } else {
-        //         echo 'Success';
-        //     }
+            if (!$mail->send()) {
+                echo 'Verification code sending failed';
+            } else {
+                echo 'Success';
+            }
 
     }else{
         echo ("Invalid Email address");
