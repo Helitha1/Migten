75c9d29bac4af8133ebea7054c19c6fa441b33dd
Yaso <yasogaran@gmail.com>
2023-10-03 22:05:57 +0530

    Update header2.php

--- header2.php
+++ header2.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=7" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="icon" type="image/png" href="./res/logo-sm.png" />
-    <link rel="stylesheet" href="/main.css" />
+    <link rel="stylesheet" href="main.css" />
     <link rel="stylesheet" href="bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
     <link rel="stylesheet" href="cdn.jsdelivr.net_npm_swiper@10.2.0_swiper-bundle.min.css" />
     <link rel="stylesheet" href="./fontawsome/css/all.css" />
